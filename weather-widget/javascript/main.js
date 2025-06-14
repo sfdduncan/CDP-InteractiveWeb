662493a59e8c1761a7a97313d89a4d3a87858c31
sfdduncan <164909361+sfdduncan@users.noreply.github.com>
2025-06-14 15:36:25 -0400

    update main.js

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,14 +1,33 @@
 console.log("this works");
 
-const API_KEY=config.WEATHER_API_KEY; 
 
+let btn = document.querySelector(".search-button");
+let input = document.querySelector(".zipcode");
 
-function getWeatherData() {
+function getWeatherData(zip) {
+    let API_ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?zip=${ZIP_CODE}&APPID=${API_KEY}`
+    let API_KEY = config.WEATHER_API_KEY;
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
-  .then(response => response.json())
-  .then(data => console.log(data));
-
+  fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      // store the data in a variable of our choosing
+      let local_weather_data = data;
+      // log the data to the browser console
+      console.log(local_weather_data);
+    });
 }
+
+
+
+function getZipCode(e) {
+    e.preventDefault(); 
+    let ZIP_CODE = input.value;
+    getWeatherData(ZIP_CODE);
+
+};
+
+btn.addEventListener('click', getZipCode);
+
 
 getWeatherData()
