46621a24855ee3bfcb648bbafe138418a8bb9d9c
sfdduncan <164909361+sfdduncan@users.noreply.github.com>
2025-06-14 15:21:15 -0400

    Added to main.js

---
+++ weather-widget/javascript/main.js
@@ -0,0 +1,14 @@
+console.log("this works");
+
+const API_KEY=config.WEATHER_API_KEY; 
+
+
+function getWeatherData() {
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+}
+
+getWeatherData()
